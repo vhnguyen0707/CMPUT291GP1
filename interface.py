55a89c70119298c297ce8f43471d9054f3191eb1
yuqian5 <28016308+yuqian5@users.noreply.github.com>
2019-10-27 04:19:55 -0600

    renewVReg completed and tested

--- interface.py
+++ interface.py
@@ -29,7 +29,7 @@ def help():
             " ",
             "   - quit              quit the program",
             " ",
-            "Example Usage: regBirth Kylo Ren male 2019/01/24 edmonton Princess Leia Han Solo")
+            "Example Usage: regBirth Kylo Ren male 2019-01-24 edmonton Princess Leia Han Solo")
     for i in info:
         print(i)
 
@@ -37,6 +37,8 @@ def help():
 class Interface:
     def __init__(self, connection, username, role, city):
         self.crsr = connection.cursor()  # create cursor
+
+        self.connection = connection
 
         # user information
         self.username = username
@@ -59,22 +61,22 @@ class Interface:
                     help()
                 elif command[0] == "quit" or command == "q":
                     break  # exit
-                elif command[0] == "regbirth".lower():
-                    self.regBirth(command[1:])
-                elif command[0] == "regmar".lower():
-                    self.regMar(command[1:])
-                elif command[0] == "renewvreg".lower():
-                    self.renewVReg(command[1:])
-                elif command[0] == "procsale".lower():
-                    self.procSale(command[1:])
-                elif command[0] == "procpay".lower():
-                    self.procPay(command[1:])
-                elif command[0] == "getabstract".lower():
-                    self.getAbstract(command[1:])
-                elif command[0] == "createticket".lower():
-                    self.createTicket(command[1:])
-                elif command[0] == "findowner".lower():
-                    self.findOwner(command[1:])
+                elif command[0] == "regbirth":
+                    self.regBirth(command)
+                elif command[0] == "regmar":
+                    self.regMar(command)
+                elif command[0] == "renewvreg":
+                    self.renewVReg(command)
+                elif command[0] == "procsale":
+                    self.procSale(command)
+                elif command[0] == "procpay":
+                    self.procPay(command)
+                elif command[0] == "getabstract":
+                    self.getAbstract(command)
+                elif command[0] == "createticket":
+                    self.createTicket(command)
+                elif command[0] == "findowner":
+                    self.findOwner(command)
             else:  # prompt that an invalid command has been entered
                 print("\"%s\" is not a valid command, type \"help\" for assistance" % " ".join(command))
 
@@ -97,7 +99,32 @@ class Interface:
             print("you do not have the privilege to perform this operation")
             return False
 
-        # TODO perform operation
+        # fetch expiry date from db
+        retData = self.crsr.execute("""SELECT expiry FROM registrations WHERE regno = ? COLLATE NOCASE""", (info[1],)).fetchall()
+        # check if a valid regno was provided, if not, prompt and return
+        if not retData:
+            print("The provided regno could not be found in the database, please try again.")
+            return
+        # convert expiry date from db into python datetime object
+        expirationDate = datetime.date(int(retData[0][0][0:4]), int(retData[0][0][5:7]), int(retData[0][0][8:10]))
+        # get current date
+        currentDate = datetime.datetime.now().date()
+        # new date to be uploaded to the db
+        newExpirationDate = None
+
+        # if the registration is already expired, set newExpirationDate to 1 year from cuurentDate
+        if expirationDate < currentDate:
+            newExpirationDate = datetime.date(currentDate.year + 1, currentDate.month, currentDate.day)
+        elif expirationDate >= currentDate: # else, add 1 year to expirationDate
+            newExpirationDate = datetime.date(expirationDate.year + 1, expirationDate.month, expirationDate.day)
+
+        # upload new date
+        self.crsr.execute("""UPDATE registrations SET expiry = ? WHERE regno = ? COLLATE NOCASE""", (newExpirationDate, info[1],))
+        self.connection.commit()  # commit the change
+
+        print("Expiration Date for Registration %d set to: %s" % (int(info[1]), str(newExpirationDate)))
+
+        return True
 
     def procSale(self, info):
         if self.role != "a":
